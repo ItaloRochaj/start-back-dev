243c1e1c23ff55dda7bbd783c09ef060a4e2ead0
Italo Rocha <italorochaj@icloud.com>
2026-02-02 18:04:09 -0300

    COMMIT BACKEND #1 - Domain Layer
    - Adicionado campo matricula na classe Student (domain)
    - Type: String
    - Propósito: Armazenar número sequencial da matrícula do aluno
    - Formato: YYYY + 5 dígitos (ex: 202600001)

--- src/main/java/start/students/core/domain/entities/Student.java
+++ src/main/java/start/students/core/domain/entities/Student.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class Student {
     private String id;
+    private String matricula;
     private String name;
     private String cpf;
     private String email;
